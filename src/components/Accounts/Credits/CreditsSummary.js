ce9bca162a1dd395bfc4e1ffd431a69eeb9abc12
Mark Satin <msatin2009@gmail.com>
2022-03-29 15:41:36 -0400

    more header refinement on mobile; styling fixes to accounts pages

--- src/components/Accounts/Credits/CreditsSummary.js
+++ src/components/Accounts/Credits/CreditsSummary.js
@@ -15,43 +15,41 @@ const creditData = {
 const CreditsSummary = () => {
   return (
     <Container>
-      <Row>
-        <Col xs={10} className={classes.summary}>
-          <div>
-            <p>Current Balance</p>
-            <h1>{usdFormatter.format(creditData.currentBalance)}</h1>
-          </div>
-          <select name='account' id='account'>
-            <option value='credit'>Purse Infinity Cash Card</option>
-          </select>
-          <Row className={classes.data}>
-            <Col xs={12} sm={6}>
-              <p>Rewards Balance</p>
-            </Col>
-            <Col xs={12} sm={6}>
-              <h5>{creditData.rewardsBalance} Points</h5>
-            </Col>
-            <Col xs={12} sm={6}>
-              <p>Available Credit</p>
-            </Col>
-            <Col xs={12} sm={6}>
-              <h5>{usdFormatter.format(creditData.availCredit)}</h5>
-            </Col>
-            <Col xs={12} sm={6}>
-              <p>Minimum Amount Due</p>
-            </Col>
-            <Col xs={12} sm={6}>
-              <h5>{usdFormatter.format(creditData.minAmountDue)}</h5>
-            </Col>
-            <Col xs={12} sm={6}>
-              <p>Payment Due Date</p>
-            </Col>
-            <Col xs={12} sm={6}>
-              <h5>{creditData.paymentDueDate}</h5>
-            </Col>
-          </Row>
-        </Col>
-      </Row>
+      <div className={classes.summary}>
+        <div>
+          <p>Current Balance</p>
+          <h1>{usdFormatter.format(creditData.currentBalance)}</h1>
+        </div>
+        <select name='account' id='account'>
+          <option value='credit'>Purse Infinity Cash Card</option>
+        </select>
+        <Row className={classes.data}>
+          <Col xs={12} sm={6}>
+            <p>Rewards Balance</p>
+          </Col>
+          <Col xs={12} sm={6}>
+            <h5>{creditData.rewardsBalance} Points</h5>
+          </Col>
+          <Col xs={12} sm={6}>
+            <p>Available Credit</p>
+          </Col>
+          <Col xs={12} sm={6}>
+            <h5>{usdFormatter.format(creditData.availCredit)}</h5>
+          </Col>
+          <Col xs={12} sm={6}>
+            <p>Minimum Amount Due</p>
+          </Col>
+          <Col xs={12} sm={6}>
+            <h5>{usdFormatter.format(creditData.minAmountDue)}</h5>
+          </Col>
+          <Col xs={12} sm={6}>
+            <p>Payment Due Date</p>
+          </Col>
+          <Col xs={12} sm={6}>
+            <h5>{creditData.paymentDueDate}</h5>
+          </Col>
+        </Row>
+      </div>
     </Container>
   );
 };
--- src/components/Layout/Header/TopNavBar.js
+++ src/components/Layout/Header/TopNavBar.js
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react';
-import { Nav } from 'react-bootstrap';
+import { Container, Row, Col, Nav } from 'react-bootstrap';
 
 import AmericanFlag from '../../../assets/Emojis/US_Flag.webp';
 import CaymansFlag from '../../../assets/Emojis/CI_Flag.webp';
@@ -34,54 +34,58 @@ const TopNavBar = () => {
   }, [showDropdown]);
 
   return (
-    <div className={classes['top-nav']}>
-      <Nav.Link href='/about'>About</Nav.Link>
-      <span>|</span>
-      <Nav.Link href='/contact'>Contact</Nav.Link>
-      <span>|</span>
-      <Nav.Link href='/languages'>Languages</Nav.Link>
-      <span>|</span>
-      <Nav.Link href='/help'>Help</Nav.Link>
-      <span>|</span>
-      <div
-        className={classes.dropdown}
-        onMouseEnter={() => setShowDropdown(true)}
-      >
-        <img src={flag} alt='Country flag' />
-        {showDropdown && (
-          <div className={dropdownClasses}>
-            <div onClick={selectFlagHandler}>
-              America
-              <img src={AmericanFlag} alt='American Flag' />
-            </div>
-            <div onClick={selectFlagHandler}>
-              Canada
-              <img src={AmericanFlag} alt='Canadian Flag' />
-            </div>
-            <div onClick={selectFlagHandler}>
-              Cayman Islands
-              <img src={CaymansFlag} alt='Caymans Flag' />
-            </div>
-            <div onClick={selectFlagHandler}>
-              Guatemala
-              <img src={AmericanFlag} alt='Guatemalan Flag' />
-            </div>
-            <div onClick={selectFlagHandler}>
-              Singapore
-              <img src={SingaporeFlag} alt='Singaporean Flag' />
-            </div>
-            <div onClick={selectFlagHandler}>
-              Switzerland
-              <img src={SwissFlag} alt='Swiss Flag' />
-            </div>
-            <div onClick={selectFlagHandler}>
-              Venezuela
-              <img src={AmericanFlag} alt='Venezuelan Flag' />
-            </div>
+    <Container>
+      <Row>
+        <Col className={classes['top-nav']}>
+          <Nav.Link href='/about'>About</Nav.Link>
+          <span>|</span>
+          <Nav.Link href='/contact'>Contact</Nav.Link>
+          <span>|</span>
+          <Nav.Link href='/languages'>Languages</Nav.Link>
+          <span>|</span>
+          <Nav.Link href='/help'>Help</Nav.Link>
+          <span>|</span>
+          <div
+            className={classes.dropdown}
+            onMouseEnter={() => setShowDropdown(true)}
+          >
+            <img src={flag} alt='Country flag' />
+            {showDropdown && (
+              <div className={dropdownClasses}>
+                <div onClick={selectFlagHandler}>
+                  America
+                  <img src={AmericanFlag} alt='American Flag' />
+                </div>
+                <div onClick={selectFlagHandler}>
+                  Canada
+                  <img src={AmericanFlag} alt='Canadian Flag' />
+                </div>
+                <div onClick={selectFlagHandler}>
+                  Cayman Islands
+                  <img src={CaymansFlag} alt='Caymans Flag' />
+                </div>
+                <div onClick={selectFlagHandler}>
+                  Guatemala
+                  <img src={AmericanFlag} alt='Guatemalan Flag' />
+                </div>
+                <div onClick={selectFlagHandler}>
+                  Singapore
+                  <img src={SingaporeFlag} alt='Singaporean Flag' />
+                </div>
+                <div onClick={selectFlagHandler}>
+                  Switzerland
+                  <img src={SwissFlag} alt='Swiss Flag' />
+                </div>
+                <div onClick={selectFlagHandler}>
+                  Venezuela
+                  <img src={AmericanFlag} alt='Venezuelan Flag' />
+                </div>
+              </div>
+            )}
           </div>
-        )}
-      </div>
-    </div>
+        </Col>
+      </Row>
+    </Container>
   );
 };
 
