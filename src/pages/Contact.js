cd79477c9392a6688bfb1d52e7b48f486ac6bf6e
Mark Satin <msatin2009@gmail.com>
2022-08-18 00:34:52 -0400

    cleaned up DOM tree

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -6,34 +6,28 @@ import ContactCard from '../components/UI/General/ContactCard';
 
 const Contact = () => {
   return (
-    <Container className='contact'>
+    <Container className='contact-page'>
+      <h1 className='contact-page--title'>Contact Us</h1>
+      <h3 className='contact-page--subtitle'>
+        <i>What seems to be the problem, dear?</i>
+      </h3>
       <Row>
-        <div>
-          <Col xs={12} md={10} className='page'>
-            <h1 className='title'>Contact Us</h1>
-            <h2 className='subtitle'>
-              <i>What seems to be the problem, dear?</i>
-            </h2>
-            <Row>
-              {ContactData.map((item) => (
-                <Col xs={12} lg={6} key={item.department}>
-                  <ContactCard department={item.department} data={item.data} />
-                </Col>
-              ))}
-            </Row>
-            <div className='location'>
-              <h2>
-                <i>Or visit us in person at one of our </i>
-              </h2>
-              <h1>
-                <Link to='/find-branch' className='link'>
-                  Fine Locations
-                </Link>
-              </h1>
-            </div>
+        {ContactData.map((item) => (
+          <Col xs={12} lg={6} key={item.department}>
+            <ContactCard department={item.department} data={item.data} />
           </Col>
-        </div>
+        ))}
       </Row>
+      <div className='contact-page--footer'>
+        <h2>
+          <i>Or visit us in person at one of our </i>
+        </h2>
+        <h1>
+          <Link to='/find-branch' className='link'>
+            Fine Locations
+          </Link>
+        </h1>
+      </div>
     </Container>
   );
 };
