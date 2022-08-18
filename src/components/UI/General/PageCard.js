929d0937d394e278c0faaf97df661b8ec2834718
Mark Satin <msatin2009@gmail.com>
2022-08-18 00:33:25 -0400

    added page margin sass var

--- src/components/UI/General/PageCard.js
+++ src/components/UI/General/PageCard.js
@@ -3,7 +3,7 @@ import { Container } from 'react-bootstrap';
 const PageCard = ({ title, subtitle, children }) => {
   return (
     <Container>
-      <div className=' d-flex flex-column justify-content-around page-card'>
+      <div className='d-flex flex-column justify-content-around page-card'>
         <h1 className='page-card--title'>{title}</h1>
         <h3 className='page-card--subtitle'>
           <i>{subtitle}</i>
