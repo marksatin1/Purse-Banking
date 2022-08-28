33dbabae27d96423bd7616e79a802aaa0066c6f5
Mark Satin <msatin2009@gmail.com>
2022-08-28 17:22:37 -0400

    Rewrote some fx's to be cleaner

--- src/helpers/functions/FormValidationFunctions.js
+++ src/helpers/functions/FormValidationFunctions.js
@@ -1,3 +1,11 @@
+// isEmpty(value)
+// minMaxLength(text, minLength, maxLength)
+// validateEmail(text)
+// userExists(email)
+// passwordStrength(text)
+// validateConfirmPassword(password, confirmPassword, formErrors)
+// formErrorValidation(errField, errValue, errArray, user, setUser, event, setSelected)
+
 import zxcvbn from 'zxcvbn';
 
 export const isEmpty = (value) => value.trim() === '';
@@ -49,5 +57,97 @@ export const validateConfirmPassword = (
   } else {
     delete formErrors.confirmPassword;
     return true;
+  }
+};
+
+export const formErrorValidation = (
+  errField,
+  errValue,
+  errArray,
+  user,
+  setUser,
+  event,
+  setSelected
+) => {
+  switch (errField) {
+    case 'firstName':
+      if (minMaxLength(errValue, 3)) {
+        errArray[errField] = 'More than 2 letters, bub!';
+      } else {
+        delete errArray[errField];
+      }
+      setUser({ ...user, firstName: errValue });
+      break;
+    case 'lastName':
+      if (minMaxLength(errValue, 3)) {
+        errArray[errField] = 'MORE than 2 letters!';
+      } else {
+        delete errArray[errField];
+      }
+      setUser({ ...user, lastName: errValue });
+      break;
+    case 'email':
+      if (!errValue || validateEmail(errValue)) {
+        errArray[errField] =
+          'Make it valid, buddy: @ sign, period - the works!';
+      } else {
+        userExists(errValue).then((result) => {
+          if (result) {
+            errArray[errField] =
+              'Looks like SOMEBODY already registered with us';
+          } else {
+            delete errArray[errField];
+          }
+        });
+      }
+      setUser({ ...user, email: errValue });
+      break;
+    case 'password':
+      if (minMaxLength(errValue, 7)) {
+        errArray[errField] = 'I want a good, clean password here, folks';
+      } else if (passwordStrength(errValue)) {
+        errArray[errField] = 'WEAK!!';
+      } else {
+        delete errArray[errField];
+      }
+      setUser({ ...user, password: errValue });
+      if (user.confirmPassword) {
+        validateConfirmPassword(errValue, user.confirmPassword, errArray);
+      }
+      break;
+    case 'confirmPassword':
+      let valid = validateConfirmPassword(user.password, errValue, errArray);
+      if (valid) {
+        setUser({ ...user, confirmPassword: errValue });
+      }
+      break;
+    case 'firstborn':
+      if (errValue === 'no') {
+        errArray[errField] = 'MUST CEDE RIGHTS TO FIRSTBORN!';
+      } else {
+        delete errArray[errField];
+      }
+      setUser({ ...user, firstborn: errValue });
+      break;
+    case 'country':
+      setSelected(errValue);
+      if (errValue === 'Anywhere Else') {
+        errArray[errField] = "You're not from around here, are you?";
+      } else {
+        delete errArray[errField];
+      }
+      setUser({ ...user, country: errValue });
+      break;
+    case 'agree':
+      if (!event.target.checked) {
+        errArray[errField] =
+          "Best be agreein' to them terms and conditions there, chap";
+      } else {
+        delete errArray[errField];
+      }
+      setUser({ ...user, agree: event.target.checked });
+      break;
+    default:
+      break;
   }
 };
