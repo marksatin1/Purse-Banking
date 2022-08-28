bfc8590147d0208954fb25d8c06ac352fa446fc3
Mark Satin <msatin2009@gmail.com>
2022-08-28 17:19:48 -0400

    Created file to org url endpoints

---
+++ src/api/endpoints.js
@@ -0,0 +1,14 @@
+// Firebase endpoints
+
+export const fbSignUpUrl =
+  'https://identitytoolkit.googleapis.com/v1/accounts:signUp';
+
+export const fbSignInUrl =
+  'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword';
+
+export const fbUsersUrl =
+  'https://react-http-841ed-default-rtdb.firebaseio.com/users.json';
+
+// CoinCap endpoint
+
+export const coinCapUrl = 'https://api.coincap.io/v2/assets';
