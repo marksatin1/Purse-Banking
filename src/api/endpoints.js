8b119a61fb5180739c21869dd6c9cd84b4f9eb2d
Mark Satin <msatin2009@gmail.com>
2022-08-29 12:23:09 -0400

    Added fb bank account endpoints

--- src/api/endpoints.js
+++ src/api/endpoints.js
@@ -12,6 +12,24 @@ export const fbUsersUrl =
 export const fbCommentsUrl =
   'https://react-http-841ed-default-rtdb.firebaseio.com/comments.json';
 
+export const fbCheckingActUrl =
+  'https://react-http-841ed-default-rtdb.firebaseio.com/accounts/checking_account/activity.json';
+
+export const fbCheckingDetUrl =
+  'https://react-http-841ed-default-rtdb.firebaseio.com/accounts/checking_account/details.json';
+
+export const fbSavingsActUrl =
+  'https://react-http-841ed-default-rtdb.firebaseio.com/accounts/savings_account/activity.json';
+
+export const fbSavingsDetUrl =
+  'https://react-http-841ed-default-rtdb.firebaseio.com/accounts/savings_account/details.json';
+
+export const fbCreditsActUrl =
+  'https://react-http-841ed-default-rtdb.firebaseio.com/accounts/credit_account/activity.json';
+
+export const fbCreditsDetUrl =
+  'https://react-http-841ed-default-rtdb.firebaseio.com/accounts/credit_account/details.json';
+
 // CoinCap endpoint
 
 export const coinCapUrl = 'https://api.coincap.io/v2/assets';
