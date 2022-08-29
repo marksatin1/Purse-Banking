c45951cd953022a1676d64a9fedd11fa5dc32f7e
Mark Satin <msatin2009@gmail.com>
2022-08-28 21:31:54 -0400

    created new data file

---
+++ src/helpers/data/BankingData.js
@@ -0,0 +1,17 @@
+export const accountsSummary = {
+  checkingData: {
+    begBalance: '32725.28',
+    pending: '17800.00',
+    availBalance: '14925.28',
+  },
+  savingsData: {
+    begBalance: '64903.21',
+    pending: '2158.31',
+    availBalance: '62744.90',
+  },
+  creditData: {
+    curBalance: '362.82',
+    availCredit: '29637.18',
+    paymentDueDate: 'January 9, 2022',
+  },
+};
