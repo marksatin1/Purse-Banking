7bfab3d0cc145d1fdead65b5e5df71f68cc7b58e
Mark Satin <msatin2009@gmail.com>
2022-08-31 18:34:12 -0400

    converted to async/await funcs

--- src/helpers/functions/ApiFunctions.js
+++ src/helpers/functions/ApiFunctions.js
@@ -1,8 +1,9 @@
 // getSecureToken(url, email, password)
 // postUserData(user, localId)
-// getCurrentUserData(localId)
-// getDebitsData(url)
-// getCreditsData(url)
+// getCurrentUser(localId)
+// getDateSortedComments()
+// getDebitsData(endpoints)
+// getCreditsData(endpoints)
 // tokenExpiresAt(expTimeSeconds)
 // calculateRemainingTime(expTimeSeconds)
 // retrieveStoredTokenData()
@@ -10,8 +11,8 @@
 import axios from 'axios';
 import { fbUsersUrl, fbCommentsUrl } from '../data/ApiEndpoints';
 
-export const getSecureToken = (url, email, password) => {
-  return axios({
+export const getSecureToken = async (url, email, password) => {
+  const { data } = await axios({
     method: 'post',
     url,
     params: {
@@ -22,13 +23,13 @@ export const getSecureToken = (url, email, password) => {
       password,
       returnSecureToken: true,
     },
-  }).then((response) => {
-    const { idToken, expiresIn, localId } = response.data;
-    const expirationTime = tokenExpiresAt(expiresIn);
-    const signInCreds = { idToken, expirationTime, localId };
-
-    return signInCreds;
   });
+
+  const { idToken, expiresIn, localId } = data;
+  const expirationTime = tokenExpiresAt(expiresIn);
+  const signInCreds = { idToken, expirationTime, localId };
+
+  return signInCreds;
 };
 
 export const postUserData = (user, localId) => {
@@ -49,130 +50,123 @@ export const postUserData = (user, localId) => {
   });
 };
 
-export const getCurrentUser = (localId) => {
-  return axios({
-    method: 'get',
-    url: fbUsersUrl,
-  }).then((response) => {
-    const { data } = response;
-    const allUsers = [];
+export const getCurrentUser = async (localId) => {
+  const { data } = await axios.get(fbUsersUrl);
+  const allUsers = [];
 
-    for (const user in data) {
-      allUsers.push({
-        firstName: data[user].firstName,
-        lastName: data[user].lastName,
-        email: data[user].email,
-        password: data[user].password,
-        localId: data[user].localId,
-      });
-    }
-
-    const crntUser = allUsers.filter((user) => user.localId === localId);
-    return crntUser[0];
-  });
-};
-
-export const getDateSortedComments = () => {
-  return axios({
-    method: 'get',
-    url: fbCommentsUrl,
-  }).then((response) => {
-    const { data } = response;
-    const comments = [];
-
-    for (const comment in data) {
-      comments.push({
-        username: data[comment].username,
-        age: data[comment].age,
-        location: data[comment].location,
-        memberDate: data[comment].memberDate,
-        overallStars: data[comment].overallStars,
-        commentDate: data[comment].commentDate,
-        title: data[comment].title,
-        content: data[comment].content,
-        onlineStars: data[comment].onlineStars,
-        customerStars: data[comment].customerStars,
-        accountStars: data[comment].accountStars,
-        recommend: data[comment].recommend,
-      });
-    }
-    comments.sort((a, b) => new Date(b.commentDate) - new Date(a.commentDate));
-    return comments;
-  });
-};
-
-//// Test with Promise.all()
-
-export const getDebitsData = (endpoints) => {
-  return Promise.all(endpoints.map((endpoint) => axios.get(endpoint)))
-    .then((debitsData) => {
-      const [{ data: actData }, { data: detData }] = debitsData;
-      const debitsActivity = [];
-      const debitsDetails = [];
-
-      for (let activity in actData) {
-        debitsActivity.push({
-          date: actData[activity].date,
-          type: actData[activity].type,
-          description: actData[activity].description,
-          amount: actData[activity].amount,
-          balance: actData[activity].balance,
-        });
-      }
-      debitsActivity.sort((a, b) => new Date(b.date) - new Date(a.date));
-
-      for (let detail in detData) {
-        debitsDetails.push({
-          accountName: detData[detail].accountName,
-          accountNumber: detData[detail].accountNumber,
-          routeNumber: detData[detail].routeNumber,
-          interestRate: detData[detail].interestRate,
-          accruedInterest: detData[detail].accruedInterest,
-          prevStatementDate: detData[detail].prevStatementDate,
-          effectiveDate: detData[detail].effectiveDate,
-        });
-      }
-
-      return [debitsActivity, debitsDetails[0]];
-    })
-    .catch((error) => {
-      console.log(error);
+  for (const user in data) {
+    allUsers.push({
+      firstName: data[user].firstName,
+      lastName: data[user].lastName,
+      email: data[user].email,
+      password: data[user].password,
+      localId: data[user].localId,
     });
+  }
+  const crntUser = allUsers.filter((user) => user.localId === localId);
+
+  return crntUser[0];
 };
 
-export const getCreditsData = (endpoints) => {
-  return Promise.all(endpoints.map((endpoint) => axios.get(endpoint))).then(
-    (creditsData) => {
-      const [{ data: actData }, { data: detData }] = creditsData;
-      const creditsActivity = [];
-      const creditsDetails = [];
+export const getDateSortedComments = async () => {
+  const { data } = await axios.get(fbCommentsUrl);
+  const comments = [];
 
-      for (let activity in actData) {
-        creditsActivity.push({
-          date: actData[activity].date,
-          description: actData[activity].description,
-          amount: actData[activity].amount,
-        });
-      }
-      creditsActivity.sort((a, b) => new Date(b.date) - new Date(a.date));
+  for (const comment in data) {
+    comments.push({
+      username: data[comment].username,
+      age: data[comment].age,
+      location: data[comment].location,
+      memberDate: data[comment].memberDate,
+      overallStars: data[comment].overallStars,
+      commentDate: data[comment].commentDate,
+      title: data[comment].title,
+      content: data[comment].content,
+      onlineStars: data[comment].onlineStars,
+      customerStars: data[comment].customerStars,
+      accountStars: data[comment].accountStars,
+      recommend: data[comment].recommend,
+    });
+  }
+  comments.sort((a, b) => new Date(b.commentDate) - new Date(a.commentDate));
 
-      for (let detail in detData) {
-        creditsDetails.push({
-          accountName: detData[detail].accountName,
-          accountNumber: detData[detail].accountNumber,
-          creditLimit: detData[detail].creditLimit,
-          prevStatementBalance: detData[detail].prevStatementBalance,
-          prevStatementDate: detData[detail].prevStatementDate,
-          lastPaymentAmount: detData[detail].lastPaymentAmount,
-          lastPaymentReceived: detData[detail].lastPaymentReceived,
-          interestRate: detData[detail].interestRate,
-          effectiveDate: detData[detail].effectiveDate,
-        });
-      }
+  return comments;
+};
 
-      return [creditsActivity, creditsDetails[0]];
+export const getDebitsData = async (endpoints) => {
+  try {
+    const debitsData = await Promise.all(
+      endpoints.map((endpoint) => axios.get(endpoint))
+    );
+    const [{ data: actData }, { data: detData }] = debitsData;
+
+    const debitsActivity = [];
+    const debitsDetails = [];
+
+    for (let activity in actData) {
+      debitsActivity.push({
+        date: actData[activity].date,
+        type: actData[activity].type,
+        description: actData[activity].description,
+        amount: actData[activity].amount,
+        balance: actData[activity].balance,
+      });
     }
-  );
+    debitsActivity.sort((a, b) => new Date(b.date) - new Date(a.date));
+
+    for (let detail in detData) {
+      debitsDetails.push({
+        accountName: detData[detail].accountName,
+        accountNumber: detData[detail].accountNumber,
+        routeNumber: detData[detail].routeNumber,
+        interestRate: detData[detail].interestRate,
+        accruedInterest: detData[detail].accruedInterest,
+        prevStatementDate: detData[detail].prevStatementDate,
+        effectiveDate: detData[detail].effectiveDate,
+      });
+    }
+    return [debitsActivity, debitsDetails[0]];
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+export const getCreditsData = async (endpoints) => {
+  try {
+    const creditsData = await Promise.all(
+      endpoints.map((endpoint) => axios.get(endpoint))
+    );
+    const [{ data: actData }, { data: detData }] = creditsData;
+
+    const creditsActivity = [];
+    const creditsDetails = [];
+
+    for (let activity in actData) {
+      creditsActivity.push({
+        date: actData[activity].date,
+        description: actData[activity].description,
+        amount: actData[activity].amount,
+      });
+    }
+    creditsActivity.sort((a, b) => new Date(b.date) - new Date(a.date));
+
+    for (let detail in detData) {
+      creditsDetails.push({
+        accountName: detData[detail].accountName,
+        accountNumber: detData[detail].accountNumber,
+        creditLimit: detData[detail].creditLimit,
+        prevStatementBalance: detData[detail].prevStatementBalance,
+        prevStatementDate: detData[detail].prevStatementDate,
+        lastPaymentAmount: detData[detail].lastPaymentAmount,
+        lastPaymentReceived: detData[detail].lastPaymentReceived,
+        interestRate: detData[detail].interestRate,
+        effectiveDate: detData[detail].effectiveDate,
+      });
+    }
+    return [creditsActivity, creditsDetails[0]];
+  } catch (error) {
+    console.error(error);
+  }
 };
 
 // Convert expiresIn to ms and add to current time
