9d7ed127a2b5c3749ab88362048d2aaeb28ad1c1
Mark Satin <msatin2009@gmail.com>
2022-08-31 12:14:56 -0400

    Wrapped content in <tbody>

--- src/components/UI/Accounts/CreditsDetails.js
+++ src/components/UI/Accounts/CreditsDetails.js
@@ -5,34 +5,36 @@ import Table from 'react-bootstrap/Table';
 const CreditsDetails = ({ details }) => {
   return (
     <Table borderless className='credits-details'>
-      <tr>
-        <th>Account Name</th>
-        <td>{details.accountName}</td>
-      </tr>
-      <tr>
-        <th>Account Number</th>
-        <td>{details.accountNumber}</td>
-      </tr>
-      <tr>
-        <th>Credit Limit</th>
-        <td>{details.creditLimit}</td>
-      </tr>
-      <tr>
-        <th>Previous Statement Balance</th>
-        <td>{usdFormatter(details.prevStatementBalance)}</td>
-      </tr>
-      <tr>
-        <th>Previous Statement Date</th>
-        <td>{details.prevStatementDate}</td>
-      </tr>
-      <tr>
-        <th>Last Payment Amount</th>
-        <td>{usdFormatter(details.lastPaymentAmount)}</td>
-      </tr>
-      <tr>
-        <th>Last Payment Received</th>
-        <td>{details.lastPaymentReceived}</td>
-      </tr>
+      <tbody>
+        <tr>
+          <th>Account Name</th>
+          <td>{details.accountName}</td>
+        </tr>
+        <tr>
+          <th>Account Number</th>
+          <td>{details.accountNumber}</td>
+        </tr>
+        <tr>
+          <th>Credit Limit</th>
+          <td>{details.creditLimit}</td>
+        </tr>
+        <tr>
+          <th>Previous Statement Balance</th>
+          <td>{usdFormatter(details.prevStatementBalance)}</td>
+        </tr>
+        <tr>
+          <th>Previous Statement Date</th>
+          <td>{details.prevStatementDate}</td>
+        </tr>
+        <tr>
+          <th>Last Payment Amount</th>
+          <td>{usdFormatter(details.lastPaymentAmount)}</td>
+        </tr>
+        <tr>
+          <th>Last Payment Received</th>
+          <td>{details.lastPaymentReceived}</td>
+        </tr>
+      </tbody>
     </Table>
   );
 };
--- src/components/UI/Accounts/DebitsDetails.js
+++ src/components/UI/Accounts/DebitsDetails.js
@@ -3,34 +3,36 @@ import Table from 'react-bootstrap/Table';
 const DebitsDetails = ({ details }) => {
   return (
     <Table borderless className='credits-details'>
-      <tr>
-        <th>Account Name</th>
-        <td>{details.accountName}</td>
-      </tr>
-      <tr>
-        <th>Account Number</th>
-        <td>{details.accountNumber}</td>
-      </tr>
-      <tr>
-        <th>Routing Number</th>
-        <td>{details.routeNumber}</td>
-      </tr>
-      <tr>
-        <th>Interest Rate</th>
-        <td>{details.interestRate}&#x25;</td>
-      </tr>
-      <tr>
-        <th>Accrued Interest YTD</th>
-        <td>{details.accruedInterest}</td>
-      </tr>
-      <tr>
-        <th>Previous Statement Date</th>
-        <td>{details.prevStatementDate}</td>
-      </tr>
-      <tr>
-        <th>Effective Date</th>
-        <td>{details.effectiveDate}</td>
-      </tr>
+      <tbody>
+        <tr>
+          <th>Account Name</th>
+          <td>{details.accountName}</td>
+        </tr>
+        <tr>
+          <th>Account Number</th>
+          <td>{details.accountNumber}</td>
+        </tr>
+        <tr>
+          <th>Routing Number</th>
+          <td>{details.routeNumber}</td>
+        </tr>
+        <tr>
+          <th>Interest Rate</th>
+          <td>{details.interestRate}&#x25;</td>
+        </tr>
+        <tr>
+          <th>Accrued Interest YTD</th>
+          <td>{details.accruedInterest}</td>
+        </tr>
+        <tr>
+          <th>Previous Statement Date</th>
+          <td>{details.prevStatementDate}</td>
+        </tr>
+        <tr>
+          <th>Effective Date</th>
+          <td>{details.effectiveDate}</td>
+        </tr>
+      </tbody>
     </Table>
   );
 };
