9a958ec5ca67dbf9bc1831d4ef396a12956131c5
Mark Satin <msatin2009@gmail.com>
2022-09-02 17:48:46 -0400

    Updated nav/dropdown styles

--- src/components/Layout/MainNav.js
+++ src/components/Layout/MainNav.js
@@ -7,60 +7,57 @@ const MainNav = () => {
         <Nav.Item className='main-nav--item'>
           <h2>Learning</h2>
           <div className='main-nav--dropdown-content'>
-            <Nav.Link className='link no-strike' href='/crypto'>
-              <p>Crypto</p>
+            <Nav.Link href='/crypto'>
+              <p className='valid-link'>Crypto</p>
             </Nav.Link>
-            <Nav.Link className='link strike' href='#'>
-              <p>NFTs</p>
+            <Nav.Link href='#'>
+              <p className='broken-link'>NFTs</p>
             </Nav.Link>
-            <Nav.Link className='link strike' href='#'>
-              <p>Finance 101</p>
+            <Nav.Link href='#'>
+              <p className='broken-link'>Decentralized Banking</p>
             </Nav.Link>
-            <Nav.Link className='link strike' href='#'>
-              <p>Joint Bank Accounts</p>
+            <Nav.Link href='#'>
+              <p className='broken-link'>Digital Payments</p>
             </Nav.Link>
-            <Nav.Link className='link strike' href='#'>
-              <p>Digital Payments</p>
+            <Nav.Link href='#'>
+              <p className='broken-link'>Finance 101</p>
             </Nav.Link>
           </div>
         </Nav.Item>
         <Nav.Item className='main-nav--item'>
           <h2>Services</h2>
           <div className='main-nav--dropdown-content'>
-            <Nav.Link className='link strike' href='#'>
-              <p>Mobile Banking</p>
+            <Nav.Link href='#'>
+              <p className='broken-link'>Mobile App</p>
             </Nav.Link>
-            <Nav.Link className='link strike' href='#'>
-              <p>Student Checking</p>
+            <Nav.Link href='#'>
+              <p className='broken-link'>Student Checking</p>
             </Nav.Link>
-            <Nav.Link className='link strike' href='#'>
-              <p>Travel Tips</p>
+            <Nav.Link href='#'>
+              <p className='broken-link'>International Banking</p>
             </Nav.Link>
-            <Nav.Link className='link strike' href='#'>
-              <p>Overdraft Services</p>
+            <Nav.Link href='#'>
+              <p className='broken-link'>Overdraft Protection</p>
             </Nav.Link>
-            <Nav.Link className='link strike' href='#'>
-              <p>New to Purse</p>
+            <Nav.Link href='#'>
+              <p className='broken-link'>Travel Tips</p>
             </Nav.Link>
           </div>
         </Nav.Item>
         <Nav.Item className='main-nav--item'>
           <h2>Products</h2>
           <div className='main-nav--dropdown-content'>
-            <Nav.Link className='link strike' href='#'>
-              <p>CDs</p>
+            <Nav.Link href='#'>
+              <p className='broken-link'>CDs</p>
             </Nav.Link>
-            <Nav.Link
-              className='link no-strike'
-              href='/credit-offer?card=travel-card'
-            >
-              <p>Credit Cards</p>
+            <Nav.Link href='/credit-offer?card=travel-card'>
+              <p className='valid-link'>Credit Cards</p>
             </Nav.Link>
-            <Nav.Link className='link no-strike' href='/crypto-wallet'>
-              <p>Crypto Wallet</p>
+            <Nav.Link href='/crypto-wallet'>
+              <p className='broken-link'>Crypto Wallet</p>
             </Nav.Link>
-            <Nav.Link className='link strike' href='#'>
-              <p>Stock Wallet</p>
+            <Nav.Link href='#'>
+              <p className='broken-link'>Stock Portfolio</p>
             </Nav.Link>
           </div>
         </Nav.Item>
--- src/components/Layout/MyPurseButton.js
+++ src/components/Layout/MyPurseButton.js
@@ -1,6 +1,7 @@
 import { useState, useContext, useEffect } from 'react';
 
 import Nav from 'react-bootstrap/Nav';
+import NavDropdown from 'react-bootstrap/NavDropdown';
 import AuthContext from '../../context/auth-context';
 
 const MyPurseButton = () => {
@@ -10,33 +11,34 @@ const MyPurseButton = () => {
 
   useEffect(() => {
     if (authCtx.isSignedIn) {
-      setDestination({ destUrl: '/accounts', destName: 'My Purse' });
+      setDestination({ destUrl: '/my-purse/accounts', destName: 'My Purse' });
     } else if (!authCtx.isSignedIn) {
       setDestination({ destUrl: '/register', destName: 'Register' });
     }
   }, [authCtx]);
 
   return (
-    <div className='d-flex justify-content-end'>
-      <Nav className='my-purse-btn'>
+    <Nav className='my-purse-btn'>
+      <button className='my-purse-btn--btn'>
         <Nav.Link href={destination.destUrl}>
           <p>{destination.destName}</p>
         </Nav.Link>
-        {authCtx.isSignedIn && (
-          <div className='my-purse-btn--content-container'>
-            <Nav.Link href='accounts' className='link'>
-              My Accounts
-            </Nav.Link>
-            <Nav.Link href='user-settings' className='link'>
-              User Settings
-            </Nav.Link>
-            <button className='btn--sign-out' onClick={authCtx.signOut}>
-              Sign Out
-            </button>
-          </div>
-        )}
-      </Nav>
-    </div>
+      </button>
+      {authCtx.isSignedIn && (
+        <div className='my-purse-btn--dropdown-content'>
+          <Nav.Link href='accounts'>
+            <p>My Accounts</p>
+          </Nav.Link>
+          <Nav.Link href='user-settings'>
+            <p>User Settings</p>
+          </Nav.Link>
+          <NavDropdown.Divider />
+          <Nav.Link onClick={authCtx.signOut}>
+            <p className='sign-out'>Sign Out</p>
+          </Nav.Link>
+        </div>
+      )}
+    </Nav>
   );
 };
 
